6aaeee93254285f4839cea35dd2de990593f5726
daviromao <davi.romao0210@gmail.com>
2022-05-20 13:58:44 -0300

    customizing django admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from reviews.models import Category, Company, Product, ProductSite, ProductSize, Comment
+
+admin.site.register(Category)
+admin.site.register(Company)
+admin.site.register(ProductSize)
+admin.site.register(ProductSite)
+admin.site.register(Comment)
+
+admin.site.unregister(Group)
+
+admin.site.site_header = "Product Review Admin"
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'content')
