11c7d688fd77bd097c5be8bed4fee8e11d6b9f7b
daviromao <davi.romao0210@gmail.com>
2022-05-25 14:52:58 -0300

    added view to change password and update profile

--- auth/views.py
+++ auth/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import User
 
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
+from .serializers import ChangePasswordSerializer, MyTokenObtainPairSerializer, RegisterSerializer, UpdateUserSerializer
 
 from rest_framework import generics
 
@@ -17,3 +17,15 @@ class RegisterView(generics.CreateAPIView):
   queryset = User.objects.all()
   permission_classes = (AllowAny,)
   serializer_class = RegisterSerializer
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+  queryset = User.objects.all()
+  permission_classes = (IsAuthenticated,)
+  serializer_class = ChangePasswordSerializer
+
+
+class UpdateProfileView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UpdateUserSerializer
